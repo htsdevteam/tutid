56e592c8df6944f789ce3237d4632656b02d90c1
serge <email@email.com>
2017-12-27 19:22:33 +0300

    Added 1) EF; 2) Startup;

---
+++ IdentityFromScratch/IdentityFromScratch/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(IdentityFromScratch.Startup))]
+namespace IdentityFromScratch
+{
+    public class Startup
+    {
+        public void Configuration(Owin.IAppBuilder app)
+        {
+
+        }
+    }
+}
