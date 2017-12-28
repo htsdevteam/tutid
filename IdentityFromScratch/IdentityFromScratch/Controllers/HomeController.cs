d2cbe2a0a4c419a6a7cefedce4a752a15cd08f79
serge <email@email.com>
2017-12-28 19:00:05 +0300

    Added a migration for CustomUser

--- IdentityFromScratch/IdentityFromScratch/Controllers/HomeController.cs
+++ IdentityFromScratch/IdentityFromScratch/Controllers/HomeController.cs
@@ -12,17 +12,28 @@ namespace IdentityFromScratch.Controllers
         public async Task<ActionResult> Index()
         {
             var context = new ApplicationDbContext();
-            var store = new UserStore<IdentityUser>(context);
-            var manager = new UserManager<IdentityUser>(store);
+            var store = new UserStore<CustomUser>(context);
+            var manager = new UserManager<CustomUser>(store);
 
             string email = "foo@bar.com";
             string password = "Passw0rd";
-            IdentityUser user = await manager.FindByEmailAsync(email);
+            CustomUser user = await manager.FindByEmailAsync(email);
 
             if (user == null)
             {
-                user = new IdentityUser { UserName = email, Email = email };
+                user = new CustomUser {
+                    FirstName = "Super",
+                    LastName = "Admin",
+                    UserName = email,
+                    Email = email
+                };
                 await manager.CreateAsync(user, password);
+            }
+            else
+            {
+                user.FirstName = "Super";
+                user.LastName = "Admin";
+                await manager.UpdateAsync(user);
             }
             return Content("Hello Index");
         }
--- IdentityFromScratch/IdentityFromScratch/Models/IdentityModels.cs
+++ IdentityFromScratch/IdentityFromScratch/Models/IdentityModels.cs
@@ -2,8 +2,15 @@
 
 namespace IdentityFromScratch.Models
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
     {
         public ApplicationDbContext() : base() { }
     }
+
+    public class CustomUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
 }
